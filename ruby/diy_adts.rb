8579372e62ceb26b6c98a7c18d75e4e17ae2dba6
Samuel Mak <samuelswmak@gmail.com>
2016-09-15 22:30:40 -0700

    created Stack class

--- ruby/diy_adts.rb
+++ ruby/diy_adts.rb
@@ -1,2 +1,28 @@
 #homework w1d4
 #abstract data types
+
+class Stack
+  def initialize
+    # create ivar to store stack here!
+    @stack = []
+  end
+
+  def add(el)
+    # adds an element to the stack
+    @stack << el
+  end
+
+  def remove
+    # removes one element from the stack
+    @stack.pop
+  end
+
+  def show
+    # return a COPY of the stack
+    @stack.dup
+  end
+end
+
+#write a Queue class.
+#We will need the following instance methods:
+#enqueue(el), dequeue, and show
