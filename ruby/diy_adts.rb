7a13d6f4de88f8540b74b0674a70b27442a33224
Samuel Mak <samuelswmak@gmail.com>
2016-09-15 23:08:22 -0700

    created Map class

--- ruby/diy_adts.rb
+++ ruby/diy_adts.rb
@@ -46,3 +46,38 @@ class Queue
     @queue.dup
   end
 end
+
+
+#my_map = [[k1, v1], [k2, v2], [k3, v2], ...]
+class Map
+  def initialize
+    @map = []
+  end
+  #assign method can be used to either
+  #create a new key-value pair
+  #or update the value for a pre-existing key
+  #iterate through map looking for the index
+  #of the pair whose key == incoming key
+  def assign(key, value)
+    pair_idx = @map.index { |pair| pair[0] == key}
+    if pair_idx.nil?
+      @map << [key, value]
+    else
+      @map[pair_idx][1] = value
+    end
+    [key, value]
+  end
+
+  def lookup(key) #returns value of key that is being looked up
+    @map.each do { |pair| return pair[1] if pair[0] == key }
+  end
+
+  def remove(key)
+    @map.reject! { |pair| pair[0] == key }
+    nil
+  end
+
+  def show
+    @map.dup
+  end
+end
