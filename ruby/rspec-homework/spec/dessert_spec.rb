9934ad48b3c0a6ce1df4792f768065d0cb1b5d1e
Samuel Mak <samuelswmak@gmail.com>
2016-09-20 23:19:21 -0700

    fixed dessert_specs

--- ruby/rspec-homework/spec/dessert_spec.rb
+++ ruby/rspec-homework/spec/dessert_spec.rb
@@ -14,9 +14,13 @@ describe Dessert do
       expect(brownie.type).to eq("brownie")
     end
 
-    it "sets a quantity"
+    it "sets a quantity" do
+      expect(brownie.quantity).to eq(100)
+    end
 
-    it "starts ingredients as an empty array"
+    it "starts ingredients as an empty array" do
+      expect(brownie.ingredients).to be_empty
+    end
 
     it "raises an argument error when given a non-integer quantity" do
       expect { Dessert.new("cake", "tons") }.to raise_error(ArgumentError)
@@ -24,16 +28,34 @@ describe Dessert do
   end
 
   describe "#add_ingredient" do
-    it "adds an ingredient to the ingredients array"
+    it "adds an ingredient to the ingredients array" do
+      brownie.add_ingredient("chocolate chips")
+      expect(brownie.ingredients).to include("chocolate chips")
+    end
   end
 
   describe "#mix!" do
-    it "shuffles the ingredient array"
+    it "shuffles the ingredient array" do
+      ingredients = ["chocolate", "egg", "sugar", "butter"]
+
+      ingredients.each do |ingredient|
+      end
+
+      expect(brownie.ingredients).to eq(ingredients)
+      brownie.mix!
+      expect(brownie.ingredients).not_to eq(ingredients)
+      expect(brownie.ingredients.sort).to eq(ingredients.sort)
+    end
   end
 
   describe "#eat" do
-    it "subtracts an amount from the quantity"
+    it "subtracts an amount from the quantity" do
+      brownie.eat(80)
+      expect(brownie.quantity).to eq(20)
+    end
 
-    it "raises an error if the amount is greater than the quantity"
+    it "raises an error if the amount is greater than the quantity" do
+      expect { brown.eat(120)}.to raise_error("not enough left!")
+    end
   end
 end
